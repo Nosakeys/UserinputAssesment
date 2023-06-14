b6041e809ab834f76d54e622ec073598f58d5b97
Aigbe Silas <93807058+Nosakeys@users.noreply.github.com>
2023-06-14 15:04:02 +0100

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import UserInput from './Component/UserInput'
 import UserOutput from './Component/UserOutput'
+import { useState } from 'react'
 const App = () => {
+
+  // using Hook
+  const [username, setfirst] = useState('Tobi')
+
+  const changeEventHandler = (e) => {
+    setfirst(e.target.value)
+  }
   return (
     // outputing the components
+
+
+
    <div className='container'>
-   <UserInput />
-   <UserOutput/>
-   <UserOutput/>
-   <UserOutput/>
-   <UserOutput/>
-   
+   <UserInput username= {changeEventHandler}  />
+   <UserOutput username= {username} />
    </div>
   )
 }
